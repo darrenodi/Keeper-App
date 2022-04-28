911eed37ec0839e641b9a48803c1237aa04a2faa
darrenodi <tantheta85@gmail.com>
2022-04-28 22:16:58 +0100

    Create app view template

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note(){
+  return(
+    <div className="note">
+      <h1>Notes keeper app</h1>
+      <p>A really interesting note</p>
+    </div>
+  )
+}
+
+export default Note;
