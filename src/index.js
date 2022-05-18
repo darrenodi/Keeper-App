dc3743f468724bccdbefd8094bc37548cd14bd90
Darren Odi <41170585+darrenodi@users.noreply.github.com>
2022-05-18 22:39:41 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import {useState} from "react" 
 
 ReactDOM.render(<App />, document.getElementById("root"));
